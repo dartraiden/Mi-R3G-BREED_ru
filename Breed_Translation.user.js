d3aa8d6f324df2c988bc9f9f36d3fb2168d41b30
dartraiden <wowemuh@gmail.com>
2019-03-10 01:33:40 +0300

    Fix name and description

--- Breed_Translation.user.js
+++ Breed_Translation.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name		Breed Bootloader Russian Translation
-// @description	Перевод загрузчика Breed для Xiaomi Mi Router 3G
+// @name		BREED bootloader Russian translation
+// @description	Перевод загрузчика BREED для Xiaomi Mi Router 3G
 // @namespace	breed_mi_r3g_ru
 // @version		20190307.1
 // @author		LESHIY_ODESSA, dartraiden
