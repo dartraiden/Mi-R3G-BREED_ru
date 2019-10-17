1930518ae6cb050f3dbf533d868304aacb132e81
dartraiden <wowemuh@gmail.com>
2019-10-17 21:59:33 +0300

    Add 3 new strings (if you try to reset Padavan, but Padavan is not installed)

--- Breed_Translation.meta.js
+++ Breed_Translation.meta.js
@@ -2,7 +2,7 @@
 // @name		BREED bootloader Russian translation
 // @description	Перевод загрузчика BREED для Xiaomi Mi Router 3G
 // @namespace	breed_mi_r3g_ru
-// @version		20190307.1
+// @version		20191017.1
 // @author		LESHIY_ODESSA, dartraiden
 // @include		http://192.168.1.1/*
 // ==/UserScript==
