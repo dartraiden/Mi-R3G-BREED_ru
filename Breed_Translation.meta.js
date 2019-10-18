2be5ac8d111e03a717bfb397454baea791a2aa30
dartraiden <wowemuh@gmail.com>
2019-10-18 15:19:11 +0300

    Add 4 strings (thx to Phisik from Greasy Fork)

--- Breed_Translation.meta.js
+++ Breed_Translation.meta.js
@@ -2,7 +2,7 @@
 // @name		BREED bootloader Russian translation
 // @description	Перевод загрузчика BREED для Xiaomi Mi Router 3G
 // @namespace	breed_mi_r3g_ru
-// @version		20191017.1
+// @version		20191018.1
 // @author		LESHIY_ODESSA, dartraiden
 // @include		http://192.168.1.1/*
 // ==/UserScript==
